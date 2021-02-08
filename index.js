8afb1e7563b588920ec8d26a688eaf51597c6b2b
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-02-08 15:43:48 +0100

    added html, cors e public folder

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const postsRoute = require("./routes/posts");
+const cors = require("cors");
 const app = express();
 
 //MIDDLEWARE
 //TRIGGERED WHEN WE ARE URL/POSTS ONLY
 app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
 app.use("/posts", postsRoute);
 
 //DB CONNECTION
@@ -19,6 +22,10 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => {
   console.log("Connected to the DB");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
 
 //PORT
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../schemas/Post");
 
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (err) {
+    res.json({ id: req.params.id, message: err.message });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
     const posts = await Post.find();
@@ -22,6 +31,34 @@ router.post("/", async (req, res) => {
     res.json(newPost);
   } catch (err) {
     res.json({ message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    post.remove();
+    res.json({ message: "Post was successfully removed" });
+  } catch (err) {
+    res.json({ id: req.params.id, message: err.message });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (req.body.title) {
+      post.title = req.body.title;
+      post.save();
+      res.json({ message: "Name changed successffuly" });
+    }
+    if (req.body.content) {
+      post.content = req.body.content;
+      post.save();
+      res.json({ message: "Name changed successffuly" });
+    }
+  } catch (err) {
+    res.json({ id: req.params.id, message: err.message });
   }
 });
 
